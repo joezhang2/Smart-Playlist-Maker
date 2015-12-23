ac5950d5691464d2fef725de9a946c4219e76500
Joe Zhang <joe.zhang2@gmail.com>
2015-12-22 22:04:12 -0600

    Tests fileInput and fileOutput classes

---
+++ FileIOTester.java
@@ -0,0 +1,36 @@
+public class FileIOTester{
+	final String INPUT_FILE = "testFile.txt";
+	final String FILE_OUTPUT = "testFileOuput.txt";
+	
+	FileInput inputTest;
+	FileOutput outputTest;
+	
+	public FileIOTester(){
+		inputTest = new FileInput(INPUT_FILE);
+		outputTest = new FileOutput(FILE_OUTPUT);
+	}
+
+	public FileIOTester(String inputFile, String outputFile){
+		inputTest = new FileInput(inputFile);
+		outputTest = new FileOutput(outputFile);
+	}
+	public FileOutput getOutputTest(){
+		return outputTest;
+	}
+	public FileInput getInputTest(){
+		return inputTest;
+	}
+	
+	public void copyInputToOutput(){
+		inputTest.readFile();
+		outputTest.setTempList(inputTest.getTempList());
+		outputTest.createList();
+	}
+
+	public static void main(String[] args){
+		FileIOTester tester = new FileIOTester();
+		tester.copyInputToOutput();
+		System.out.println("Contents of copied generic list");
+		tester.getInputTest().getTempList().printContent();
+	}
+}
